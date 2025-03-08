74e4f680f673bf7a0fbc4c8dc325d46b5d1e6c8a
Sothyra12 <schan256@my.centennialcollege.ca>
2025-03-07 19:25:40 -0500

    added elements in html file and created empty arrow functions in JS file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // script.js
+
+const pokemonAPI = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const characteristicContainer = document.getElementById("pokemon-characteristic");
+const pokemonStat = document.getElementById("stats");
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(pokemonAPI);
+        const data = await res.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const displayPokemon = () => {};
+
+searchBtn.addEventListener("click", displayPokemon);
